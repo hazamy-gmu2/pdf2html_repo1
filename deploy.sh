b6f01f5e56e5b67b9845045e887d2b74c020f1a3
hazamy-gmu2 <165522496+hazamy-gmu2@users.noreply.github.com>
2025-09-29 12:31:27 -0400

    Update deploy.sh
    updated branch name to test-deployment-1

--- deploy.sh
+++ deploy.sh
@@ -420,13 +420,13 @@ EOF
         BUILD_IMAGE="aws/codebuild/amazonlinux-x86_64-standard:5.0"
         COMPUTE_TYPE="BUILD_GENERAL1_SMALL"
         PRIVILEGED_MODE="false"
-        SOURCE_VERSION="test-deploy-be0"  # Use pdf2html-subtree since buildspec only exists there
+        SOURCE_VERSION="test-deployment-1"  # Use pdf2html-subtree since buildspec only exists there
         BUILDSPEC_FILE="buildspec-unified.yml"
     else
         BUILD_IMAGE="aws/codebuild/amazonlinux2-x86_64-standard:5.0"
         COMPUTE_TYPE="BUILD_GENERAL1_LARGE"
         PRIVILEGED_MODE="true"
-        SOURCE_VERSION="test-deploy-be0"
+        SOURCE_VERSION="test-deployment-1"
         BUILDSPEC_FILE="buildspec-unified.yml"
     fi
 
